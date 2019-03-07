7d492ce3026362c8c7a8a03e6e48be0e8692885e
S16T271\matubara <s16t271@stu.kagawa-u.ac.jp>
2019-03-07 18:07:40 +0900

    with as

--- matsubara/Chap2/knock11.py
+++ matsubara/Chap2/knock11.py
@@ -1,9 +1,14 @@
-file = open('hightemp.txt', 'r', encoding='UTF-8')
-filedata = file.read()
+# file = open('hightemp.txt', 'r', encoding='UTF-8')
+# filedata = file.read()
+# with as だとfile.close()を書かなくてよい
+with open('hightemp.txt', 'r', encoding='UTF-8') as file:
+    filedata = file.read()
 
 # タブをスペースに置換
 filedata = filedata.replace('\t', ' ')
 print(filedata)
 
-file = open('hightemp.txt', 'w', encoding='UTF-8')
-file.write(filedata)
+# # file = open('hightemp.txt', 'w', encoding='UTF-8')
+# file.write(filedata)
+with open('hightemp.txt', 'w', encoding='UTF-8') as file:
+    file.write(filedata)
