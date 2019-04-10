19a5c16176dfb31dad389670c5dc69ffdca143c0
YujiOka <s16t215@stu.kagawa-u.ac.jp>
2019-04-10 23:02:12 +0900

    add knock17

---
+++ oka/chap2/knock17.py
@@ -0,0 +1,22 @@
+# 1列目の異なり
+def diference(out):
+    first = []
+    for i in range(len(out)):
+        if out[i][0] in first:
+            continue
+        else:
+            first.append(out[i][0])
+    print(first)
+
+
+# ファイルの読み込み
+def reading(path):
+    with open(path, mode="r", encoding="utf-8") as f:
+        s = f.readlines()
+        elements = [elem.split() for elem in s]
+    return elements
+
+
+path = "C:\\Users\\16t215\\Desktop\\hightemp.txt"
+out = reading(path)
+diference(out)
