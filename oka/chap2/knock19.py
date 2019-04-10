9666221f78e0ffa006b43c68c097f48f85cdaf68
YujiOka <s16t215@stu.kagawa-u.ac.jp>
2019-04-10 23:03:13 +0900

    add knock19

---
+++ oka/chap2/knock19.py
@@ -0,0 +1,22 @@
+from itertools import groupby
+from collections import Counter
+
+# 頻度順
+def frequency(out):
+    out.sort()
+    count = Counter(out)
+    print(count)
+    # result = [(k, len(list(g))) for k, g in groupby(out)]
+
+
+# ファイルの読み込み
+def reading(path):
+    with open(path, mode="r", encoding="utf-8") as f:
+        s = f.readlines()
+        elements = [elem.split()[0] for elem in s]
+    return elements
+
+
+path = "C:\\Users\\16t215\\Desktop\\hightemp.txt"
+out = reading(path)
+frequency(out)
