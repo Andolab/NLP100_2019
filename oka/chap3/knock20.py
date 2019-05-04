d65a1178630eb0a1b2bf9940874e584bf789f342
YujiOka <s16t215@stu.kagawa-u.ac.jp>
2019-05-04 23:14:44 +0900

    add knock20

--- oka/chap3/knock20.py
+++ oka/chap3/knock20.py
@@ -1,2 +1,14 @@
-#こんにちは
-print("hello")
+import json
+import gzip
+# import codecs
+# from collections import OrderedDict
+# import pprint
+
+
+path = "C:\\Users\\16t215\\Desktop\\jawiki-country.json.gz"
+with gzip.open(path, mode="rt", encoding="utf-8") as f:
+    for line in f:
+        data_json = json.loads(line)
+        if data_json["title"] == "イギリス":
+            print(data_json["text"])
+            break
