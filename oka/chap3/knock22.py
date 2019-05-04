5368274a742ebc1df924ad32c13b1882d4cd2146
YujiOka <s16t215@stu.kagawa-u.ac.jp>
2019-05-04 23:17:26 +0900

    add knock22

---
+++ oka/chap3/knock22.py
@@ -0,0 +1,20 @@
+import json
+import gzip
+import re
+# import codecs
+# from collections import OrderedDict
+# import pprint
+
+
+path = "C:\\Users\\16t215\\Desktop\\jawiki-country.json.gz"
+with gzip.open(path, mode="rt", encoding="utf-8") as f:
+    for line in f:
+        data_json = json.loads(line)
+        if data_json["title"] == "イギリス":
+            text = data_json["text"]
+            break
+text_list = text.split("\n")
+for lines in text_list:
+    name = re.search(r"(?<=\[\[Category:)(.+)(?=\]\]$)", lines)
+    if name:
+        print(name.group())
