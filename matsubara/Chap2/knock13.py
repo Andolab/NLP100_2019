8b0508a33f57a5c4ca7857ef8bc8bcf48b484ba4
S16T271\matubara <s16t271@stu.kagawa-u.ac.jp>
2019-05-25 14:07:34 +0900

    knock13 formatting

--- matsubara/Chap2/knock13.py
+++ matsubara/Chap2/knock13.py
@@ -3,4 +3,3 @@ with open("col1.txt", "r", encoding="utf-8") as f1,\
      open("marge.txt", "w", encoding="utf-8") as wfile:
     for line1, line2 in zip(f1.readlines(), f2.readlines()):
         wfile.write(line1.strip() + "\t" + line2)
-
