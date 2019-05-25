a539aa6a83b0e7ed51dd4fbda529c38eae53a113
S16T271\matubara <s16t271@stu.kagawa-u.ac.jp>
2019-05-25 14:08:32 +0900

    knock17 use set

--- matsubara/Chap2/knock17.py
+++ matsubara/Chap2/knock17.py
@@ -1,10 +1,12 @@
 with open("hightemp.txt", "r", encoding="UTF-8") as readf:
     row = readf.readlines()
-col1_list = []
-for col in row:
-    c = col.split("\t")[0]
-    if c in col1_list:
-        continue
-    else:
-        col1_list.append(c)
-print("\n".join(col1_list))
+# col1_list = []
+# for col in row:
+#     c = col.split("\t")[0]
+#     if c in col1_list:
+#         continue
+#     else:
+#         col1_list.append(c)
+# print("\n".join(col1_list))
+c = [col.split("\t")[0] for col in row]
+print("\n".join(set(c)))
