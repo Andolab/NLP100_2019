8746539ab1daaab93fe33e2422d79b9ba4b9caea
S16T271\matubara <s16t271@stu.kagawa-u.ac.jp>
2019-05-25 14:08:49 +0900

    knock18 formatting

--- matsubara/Chap2/knock18.py
+++ matsubara/Chap2/knock18.py
@@ -1,5 +1,5 @@
 with open("hightemp.txt", "r", encoding="UTF-8") as readf:
     row = readf.readlines()
-row.sort(key=lambda n:(n.split("\t")[2]), reverse=True)
+row.sort(key=lambda n: (n.split("\t")[2]), reverse=True)
 for line in row:
     print(line.strip())
