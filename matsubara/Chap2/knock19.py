e53c2d0ca65c5575aea53a5cf345981a002dbf3a
S16T271\matubara <s16t271@stu.kagawa-u.ac.jp>
2019-05-25 14:09:06 +0900

    knock19 formatting

--- matsubara/Chap2/knock19.py
+++ matsubara/Chap2/knock19.py
@@ -3,6 +3,6 @@ import collections
 with open("hightemp.txt", "r", encoding="UTF-8") as readf:
     row = readf.readlines()
 col1_list = []
-col1_list= [col.split("\t")[0] for col in row]
+col1_list = [col.split("\t")[0] for col in row]
 c = collections.Counter(col1_list)
 [print(c) for c in c.most_common()]
