f453d40c7395df8ea670624b46791ab03ad344f5
S16T271\matubara <s16t271@stu.kagawa-u.ac.jp>
2019-06-03 23:18:31 +0900

    knock23

---
+++ matsubara/Chap3/knock23.py
@@ -0,0 +1,12 @@
+import json
+import re
+
+with open("england.json", "r", encoding="UTF-8") as jsonf:
+    for line in jsonf:
+        df = json.loads(line)
+        for word in df["text"].split("\n"):
+            pattern = "==*\w.*=="
+            section = re.search(pattern, word)
+            if section is not None:
+                level = section.group().count("=") // 2 - 1
+                print(re.sub("=", "", section.group()), level)
