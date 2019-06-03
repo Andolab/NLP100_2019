1389f923585176a4b153af00e339790bc411c66f
S16T271\matubara <s16t271@stu.kagawa-u.ac.jp>
2019-06-04 00:43:57 +0900

    use function

--- matsubara/Chap3/knock25.py
+++ matsubara/Chap3/knock25.py
@@ -1,9 +1,17 @@
 import json
 import re
 
-with open("england.json", "r", encoding="UTF-8") as jsonf:
-    df = json.loads(jsonf.read())
+
+def get_eng_article(pattern: str):
+    with open("england.json", "r", encoding="UTF-8") as jsonf:
+        df = json.loads(jsonf.read())
     text = df["text"]
-    pattern1 = "(?<=\|)(.*?)\s=\s(.*?)(?=\\n\||\\n}}\\n)"
-    basic = dict(re.findall(pattern1, text))
-    print(basic)
+    prog = re.compile(pattern)
+    return prog.findall(text)
+    # return prog
+
+
+if __name__ == "__main__":
+    pattern = "(?<=\|)(.*?)\s=\s(.*?)(?=\\n\||\\n}}\\n)"
+    basic = get_eng_article(pattern)
+    print(dict(basic))
