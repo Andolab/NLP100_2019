a6239371ac22212ab0c43ba4a30101a87f0c3f3c
S16T271\matubara <s16t271@stu.kagawa-u.ac.jp>
2019-06-04 00:44:24 +0900

    use function

--- matsubara/Chap3/knock27.py
+++ matsubara/Chap3/knock27.py
@@ -1,21 +1,16 @@
-import json
+import knock25
 import re
 
-with open("england.json", "r", encoding="UTF-8") as jsonf:
-    df = json.loads(jsonf.read())
-    text = df["text"]
-    pattern1 = "(?<=\|)(.*?)\s=\s(.*?)(?=\\n\||\\n}}\\n)"
-    basic = re.findall(pattern1, text)
-
-    new_dict = {}
-    for info in basic:
-        info = list(info)
-        if "''" in info[1]:
-            info[1] = re.sub("'", "", info[1])
-        pattern2 = "\[+\W.*?\]+"
-        if re.search(pattern2, info[1]):
-            info[1] = re.sub("'|\[|\]", "", info[1])
-        new_dict[info[0]] = info[1]
+pattern1 = "(?<=\|)(.*?)\s=\s(.*?)(?=\\n\||\\n}}\\n)"
+basic = knock25.get_eng_article(pattern1)
+new_dict = {}
+for info in basic:
+    info = list(info)
+    if "''" in info[1]:
+        info[1] = re.sub("'", "", info[1])
+    pattern2 = "\[+\W.*?\]+"
+    if re.search(pattern2, info[1]):
+        info[1] = re.sub("'|\[|\]", "", info[1])
+    new_dict[info[0]] = info[1]
 
 print(new_dict)
-# [print(info) for info in new_info]
