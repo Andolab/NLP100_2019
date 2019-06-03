56dc3c7886efe4e578185e24d0accfca7dba54da
S16T271\matubara <s16t271@stu.kagawa-u.ac.jp>
2019-06-03 23:18:37 +0900

    knock24

---
+++ matsubara/Chap3/knock24.py
@@ -0,0 +1,11 @@
+import json
+import re
+
+with open("england.json", "r", encoding="UTF-8") as jsonf:
+    for line in jsonf:
+        df = json.loads(line)
+        for word in df["text"].split("\n"):
+            pattern = "(?<=File:|ファイル:)[\w,.\s]*"
+            filename = re.search(pattern, word)
+            if filename is not None:
+                print(filename.group())
