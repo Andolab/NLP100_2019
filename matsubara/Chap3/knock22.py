a77f9615b4e7065d3136f6f9aaf0a4e12c9a49ba
S16T271\matubara <s16t271@stu.kagawa-u.ac.jp>
2019-06-03 23:18:26 +0900

    knock22

---
+++ matsubara/Chap3/knock22.py
@@ -0,0 +1,11 @@
+import json
+import re
+
+with open("england.json", "r", encoding="UTF-8") as jsonf:
+    for line in jsonf:
+        df = json.loads(line)
+        for word in df["text"].split("\n"):
+            if "Category" in word:
+                pattern = "[\[\]|*]|\w*:"
+                word = re.sub(pattern, "", word)
+                print(word)
