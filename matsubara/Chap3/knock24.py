dd1c6df5793693dab6d043919eea1e7ad8a1b695
S16T271\matubara <s16t271@stu.kagawa-u.ac.jp>
2019-06-04 00:43:22 +0900

    redifine pattern

--- matsubara/Chap3/knock24.py
+++ matsubara/Chap3/knock24.py
@@ -4,8 +4,8 @@ import re
 with open("england.json", "r", encoding="UTF-8") as jsonf:
     for line in jsonf:
         df = json.loads(line)
-        for word in df["text"].split("\n"):
-            pattern = "(?<=File:|ファイル:)[\w,.\s]*"
-            filename = re.search(pattern, word)
-            if filename is not None:
-                print(filename.group())
+for word in df["text"].split("\n"):
+    pattern = "(?<=File:|ファイル:).*?(?=\|)"
+    filename = re.search(pattern, word)
+    if filename is not None:
+        print(filename.group())
