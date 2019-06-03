71b36906a9185f749f1ba6711cd8da3adcd24072
S16T271\matubara <s16t271@stu.kagawa-u.ac.jp>
2019-06-04 00:44:30 +0900

    use function

--- matsubara/Chap3/knock28.py
+++ matsubara/Chap3/knock28.py
@@ -1,20 +1,15 @@
-import json
+import knock25
 import re
 
-with open("england.json", "r", encoding="UTF-8") as jsonf:
-    df = json.loads(jsonf.read())
-    text = df["text"]
-    pattern1 = "(?<=\|)(.*?)\s=\s(.*?)(?=\\n\||\\n}}\\n)"
-    basic = re.findall(pattern1, text)
-
-    new_dict = {}
-    for info in basic:
-        info = list(info)
-        info[1] = re.sub("'|<.*>|{.*}", "", info[1])
-        pattern2 = "\[+\W.*?\]+"
-        if re.search(pattern2, info[1]):
-            info[1] = re.sub("\[|\]", "", info[1])
-        new_dict[info[0]] = info[1]
-        # print(info[1])
+pattern1 = "(?<=\|)(.*?)\s=\s(.*?)(?=\\n\||\\n}}\\n)"
+basic = knock25.get_eng_article(pattern1)
+new_dict = {}
+for info in basic:
+    info = list(info)
+    info[1] = re.sub("'|<.*>|{.*}", "", info[1])
+    pattern2 = "\[+\W.*?\]+"
+    if re.search(pattern2, info[1]):
+        info[1] = re.sub("\[|\]", "", info[1])
+    new_dict[info[0]] = info[1]
 
 print(new_dict)
