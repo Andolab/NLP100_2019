fcd7f5ca29bfaa0ec455cd9dceb3aa980eca5a70
S16T271\matubara <s16t271@stu.kagawa-u.ac.jp>
2019-10-02 23:26:17 +0900

    28 add r

--- matsubara/Chap3/knock28.py
+++ matsubara/Chap3/knock28.py
@@ -1,15 +1,15 @@
 import knock25
 import re
 
-pattern1 = "(?<=\|)(.*?)\s=\s(.*?)(?=\\n\||\\n}}\\n)"
+pattern1 = r"(?<=\|)(.*?)\s=\s(.*?)(?=\\n\||\\n}}\\n)"
 basic = knock25.get_eng_article(pattern1)
 new_dict = {}
 for info in basic:
     info = list(info)
-    info[1] = re.sub("'|<.*>|{.*}", "", info[1])
-    pattern2 = "\[+\W.*?\]+"
+    info[1] = re.sub(r"'|<.*>|{.*}", "", info[1])
+    pattern2 = r"\[+\W.*?\]+"
     if re.search(pattern2, info[1]):
-        info[1] = re.sub("\[|\]", "", info[1])
+        info[1] = re.sub(r"\[|\]", "", info[1])
     new_dict[info[0]] = info[1]
 
 print(new_dict)
